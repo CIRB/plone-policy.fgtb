84e35230068eb5769ea9dc9c6e2b6b0c833d9b0f
Stéphane Moussiaux <smoussiaux@cirb.irisnet.be>
2013-03-11 16:57:26 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+version = '1.0'
+
+setup(name='policy.fgtb',
+      version=version,
+      description="'Policy of FGTB site'",
+      long_description=open("README.rst").read() + "\n" +
+                       open("CHANGES.txt").read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='https://github.com/collective/',
+      license='gpl',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=['policy'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'Plone',
+          'Products.LinguaPlone',
+          'cirb.zopemonitoring',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
